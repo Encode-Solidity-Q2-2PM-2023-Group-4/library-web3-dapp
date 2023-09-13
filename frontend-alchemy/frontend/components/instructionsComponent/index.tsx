f797f21bf84d7ecfe65a3a76c843cbc503f1f988
linuskelsey <alfred.linus.kelsey@gmail.com>
2023-09-13 17:55:25 +0300

    Clean index.ts

--- frontend-alchemy/frontend/components/instructionsComponent/index.tsx
+++ frontend-alchemy/frontend/components/instructionsComponent/index.tsx
@@ -15,45 +15,31 @@ export default function InstructionsComponent() {
       <div className={styles.buttons_container}>
         <a
           target={"_blank"}
-          href={"https://createweb3dapp.alchemy.com/#components"}
         >
           <div className={styles.button}>
-            {/* <img src="https://static.alchemyapi.io/images/cw3d/Icon%20Medium/lightning-square-contained-m.svg" width={"20px"} height={"20px"} /> */}
             <p>Add Components</p>
           </div>
         </a>
         <a
           target={"_blank"}
-          href={"https://createweb3dapp.alchemy.com/#templates"}
         >
           <div className={styles.button}>
-            {/* <img src="https://static.alchemyapi.io/images/cw3d/Icon%20Medium/lightning-square-contained-m.svg" width={"20px"} height={"20px"} /> */}
             <p>Explore Templates</p>
           </div>
         </a>
         <a
           target={"_blank"}
-          href={"https://docs.alchemy.com/docs/create-web3-dapp"}
         >
           <div className={styles.button}>
-            <img
-              src="https://static.alchemyapi.io/images/cw3d/Icon%20Large/file-eye-01-l.svg"
-              width={"20px"}
-              height={"20px"}
-            />
             <p>Visit Docs</p>
           </div>
         </a>
         <a>
           <div className={styles.button}>
-            {/* <img src="https://static.alchemyapi.io/images/cw3d/Icon%20Medium/lightning-square-contained-m.svg" width={"20px"} height={"20px"} /> */}
             <p>Contribute</p>
           </div>
         </a>
       </div>
-      <p className={styles.get_started}>
-        Get started by editing this page in <span>/pages/index.js</span>
-      </p>
     </div>
   );
 }
