f26c2ee173b737b5282009abf02477e5e1bf3714
linuskelsey <alfred.linus.kelsey@gmail.com>
2023-09-13 12:13:07 +0300

    Edit main.ts to use swagger API

--- backend-nest/src/main.ts
+++ backend-nest/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('NFT E-Book Library')
+    .setDescription('API description for our NFT E-Book Library')
+    .setVersion('1.0')
+    .addTag('Empty')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3001);
 }
 bootstrap();
