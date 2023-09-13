03b452a99d0f10c6f035c7cfe4e299728e8a65ef
linuskelsey <alfred.linus.kelsey@gmail.com>
2023-09-13 12:20:48 +0300

    initialise post methods, src/assets and src/dtos

--- backend-nest/src/app.service.ts
+++ backend-nest/src/app.service.ts
@@ -2,7 +2,20 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  
   getHello(): string {
     return 'Hello World!';
+  }
+
+  mint(): any {
+    return true
+  }
+
+  setUser(): any {
+    return true
+  }
+
+  rent(): any {
+    return true
   }
 }
