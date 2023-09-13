4b6d35e878c55d2f2ff4bac600763cbef0cbc76a
linuskelsey <alfred.linus.kelsey@gmail.com>
2023-09-13 12:25:20 +0300

    Add constructor to app.service

--- backend-nest/src/app.service.ts
+++ backend-nest/src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { ethers } from 'ethers';
 
 @Injectable()
 export class AppService {
-  
+  bookContract: ethers.Contract;
+  provider: ethers.Provider;
+  wallet: ethers.Wallet;
+
+  constructor() {
+    this.provider = new ethers.JsonRpcProvider(
+      process.env.RPC_ENDPOINT_URL ?? "",
+    );
+    this.wallet = new ethers.Wallet(
+      process.env.PRIVATE_KEY ?? '', 
+      this.provider,
+    );
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
